8fa23b72f106902eb56393a9b5c123b1e66bb57d
jongmyung <lanto12@naver.com>
2023-12-08 20:33:51 +0900

    ✅ Test: Shutdown Thread Test

--- src/test/java/threadpool/ThreadPoolTest.java
+++ src/test/java/threadpool/ThreadPoolTest.java
@@ -12,18 +12,22 @@ public class ThreadPoolTest {
 		final Executor executor = new ThreadPool(2);
 		final int numTasks = 100;
 		final CountDownLatch latch = new CountDownLatch(100);
-		for (int i = 0; i < numTasks; i++) {
-			int finalI = i;
-			executor.execute(()->{
-				System.err.println("Thread " + Thread.currentThread().getName() + " executes a task" + finalI);
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				}
-				latch.countDown();
-			});
+		try {
+			for (int i = 0; i < numTasks; i++) {
+				int finalI = i;
+				executor.execute(() -> {
+					System.err.println("Thread " + Thread.currentThread().getName() + " executes a task" + finalI);
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);
+					}
+					latch.countDown();
+				});
+			}
+			latch.await();
+		} finally {
+			((ThreadPool)executor).shutdown();
 		}
-		latch.await();
 	}
 }
