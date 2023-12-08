4e1b5575210aa3afcdce3577c0c26e68ce2adb85
jongmyung <lanto12@naver.com>
2023-12-08 19:46:47 +0900

    ✅ Test: Minimal ThreadPool Test

---
+++ src/test/java/threadpool/ThreadPoolTest.java
@@ -0,0 +1,29 @@
+package threadpool;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+import org.junit.jupiter.api.Test;
+
+public class ThreadPoolTest {
+
+	@Test
+	void submittedTasksAreExecuted() throws InterruptedException {
+		final Executor executor = new ThreadPool(2);
+		final int numTasks = 100;
+		final CountDownLatch latch = new CountDownLatch(100);
+		for (int i = 0; i < numTasks; i++) {
+			int finalI = i;
+			executor.execute(()->{
+				System.err.println("Thread " + Thread.currentThread().getName() + " executes a task" + finalI);
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+				latch.countDown();
+			});
+		}
+		latch.await();
+	}
+}
